2a78fdc411f58ec1b7b9b4515dd9793f2dd2aafb
tamir_Shilony <tamirshilony@gmail.com>
2020-05-11 20:00:55 +0300

    tamir part

---
+++ Rand_target.m
@@ -0,0 +1,12 @@
+function[Target_shape,none_target_shape,has_target] = Rand_target(stimuli_shape)
+%stimuli_shape = ["X", "O"] ;
+check = rand(1,1);
+if check>0.5
+    Target_shape = stimuli_shape(1);
+    none_target_shape = stimuli_shape(2);
+else
+   Target_shape = stimuli_shape(2);
+   none_target_shape = stimuli_shape(1);
+end
+has_target = randperm(30);
+
