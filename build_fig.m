4ff7079f2161aeb382bd79b058434efbd386611f
tamir_Shilony <tamirshilony@gmail.com>
2020-05-12 18:44:23 +0300

    changing according the struct

--- build_fig.m
+++ build_fig.m
@@ -1,6 +1,6 @@
-function build_fig(block_type,has_target,target,non_target,target_color,non_target_color, x_vec,y_vec)
-    shapes_num = block_type{2};
-    if block_type{1} == "conj"
+function build_fig(block,has_target,target,non_target,target_color,non_target_color, x_vec,y_vec)
+    shapes_num = block.set_size;
+    if block.cond == "conj"
         conj_fig(shapes_num,has_target,target,non_target,target_color,non_target_color,x_vec,y_vec)
     else
         feat_fig(shapes_num,has_target,target,non_target,target_color,x_vec,y_vec)
