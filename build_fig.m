d66f7cd60e5044e1e9d7a2e9a43d6560fc4dad5a
tamir_Shilony <tamirshilony@gmail.com>
2020-05-12 19:44:17 +0300

    adding instraction function

--- build_fig.m
+++ build_fig.m
@@ -1,5 +1,8 @@
-function build_fig(block,has_target,target,non_target,target_color,non_target_color, x_vec,y_vec)
+function build_fig(block,has_target,target,non_target,target_color,non_target_color, x_vec,y_vec,num_of_trail)
     shapes_num = block.set_size;
+    if num_of_trail==1
+        instaction_for_first_trail(target);
+    end
     if block.cond == "conj"
         conj_fig(shapes_num,has_target,target,non_target,target_color,non_target_color,x_vec,y_vec)
     else
